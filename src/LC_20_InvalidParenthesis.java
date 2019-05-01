848fd6513007e0b273875c023cac569b3b7c3f83
satish-kandimalla <satish.kandimalla@outlook.com>
2019-04-30 18:14:20 -0700

    LC20

---
+++ src/LC_20_InvalidParenthesis.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+public class LC_20_InvalidParenthesis {
+    public boolean checkIsValid(String s){
+        HashMap<Character, Character> mapping = new HashMap<Character, Character>();
+        mapping.put(')', '(');
+        mapping.put(']', '[');
+        mapping.put('}', '{');
+        Stack<Character> st = new Stack<Character>();
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == '[' || s.charAt(i) == '{' || s.charAt(i) == '('){
+                st.push(s.charAt(i));
+            }else{
+                if(st.empty()){
+                    return false;
+                }else{
+                    Character c = st.pop();
+                    if(mapping.get(s.charAt(i)) != c){
+                        return false;
+                    }
+                }
+            }
+        }
+        if(!st.empty()){
+            return false;
+        }
+        return true;
+    }
+    public static void main(String args[]){
+        LC_20_InvalidParenthesis lc = new LC_20_InvalidParenthesis();
+        String s = "({";
+        System.out.println(lc.checkIsValid(s));
+    }
+}
